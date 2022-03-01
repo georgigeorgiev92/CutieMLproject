d78e6f25db817c8580ad28a7b04f23f13223763e
georgigeorgiev92 <34037079+georgigeorgiev92@users.noreply.github.com>
2022-03-02 00:19:37 +0100

    some minor changes

--- main_train_json.py
+++ main_train_json.py
@@ -18,7 +18,7 @@ from model_cutie2_aspp import CUTIE2 as CUTIEv2
 
 parser = argparse.ArgumentParser(description='CUTIE parameters')
 # data
-parser.add_argument('--use_cutie2', type=bool, default=True)  # True to read image from doc_path
+parser.add_argument('--use_cutie2', type=bool, default=False)  # True to read image from doc_path
 parser.add_argument('--doc_path', type=str, default='invoice_data')
 parser.add_argument('--save_prefix', type=str, default='REALINVOICES', help='prefix for ckpt')
 parser.add_argument('--test_path', type=str, default='combined') # leave empty if no test data provided
